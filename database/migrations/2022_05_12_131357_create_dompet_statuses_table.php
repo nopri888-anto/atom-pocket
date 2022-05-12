7f08a42fce7c3e3729fa41c3eed84c6f36cea6ec
unknown <nopri888anto@gmail.com>
2022-05-12 20:16:05 +0700

    Create Model

---
+++ database/migrations/2022_05_12_131357_create_dompet_statuses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDompetStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dompet_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dompet_statuses');
+    }
+}
