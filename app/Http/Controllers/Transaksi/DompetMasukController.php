686115e9b288576d3671a7ea6866caf89c97139e
unknown <nopri888anto@gmail.com>
2022-05-13 10:33:29 +0700

    Modified Transaksi Masuk

--- app/Http/Controllers/Transaksi/DompetMasukController.php
+++ app/Http/Controllers/Transaksi/DompetMasukController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Transaksi;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaksi;
 use Illuminate\Http\Request;
 
 class DompetMasukController extends Controller
@@ -14,7 +15,8 @@ class DompetMasukController extends Controller
      */
     public function index()
     {
-        //
+        $transaksis = Transaksi::all();
+        return view('transaksi.DompetMasuk.dompetMasuk', compact('transaksis'));
     }
 
     /**
